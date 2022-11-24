a9dbc2e2a3d69e6ace37bfa277fa61b6b3fbeafe
alynkt <alynkirsch@gmail.com>
2022-11-24 08:37:10 -0500

    Edit routes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import *
 
 app = Flask(__name__)
 
-@app.route('/login_page', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     return render_template("login_page.html")
 
