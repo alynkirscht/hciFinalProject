3a99ddd54f2c81755212a067dc8199059986828e
mayagriffith <mayaegriffith@gmail.com>
2022-12-05 16:36:16 -0500

    hearts are done

--- static/script.js
+++ static/script.js
@@ -14,19 +14,23 @@
 
 // }
 
-
-
-function chngimg() {
-    var img = document.getElementById('heart').src;
+function chngimg(context) {
+    var img = context.src;
     if (img){
         console.log(String(img))
     }
 
     if (String(img).includes("/static/heart-regular.svg")) {
-        document.getElementById('heart').src  = "../static/heart-solid.svg";
+        console.log("js thought this was unfilled so its making it solid")
+        context.src  = "../static/heart-solid.svg";
+        console.log("src has changed")
     }
      else {
-       document.getElementById('heart').src = '../static/heart-regular.svg';
+        console.log("js thought this was solid so its making it regular")
+        console.log(img)
+        context.src = '../static/heart-regular.svg';
+        console.log("src has changed")
    }
 
 }
+
