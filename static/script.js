080aabfa208a2616168811df91e776b74e341510
Drew Jepsen <drewjep@comcast.net>
2022-12-06 08:59:36 -0500

    added function to hide elements

--- static/script.js
+++ static/script.js
@@ -83,3 +83,12 @@ menuItems.forEach(
     menuItem.addEventListener("click", toggleMenu);
   }
 )
+
+function hideElement() {
+  var x = document.getElementById("hide");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
