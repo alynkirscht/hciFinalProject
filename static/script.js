dc31c25a260b18b74d4282d2fd185b531a506176
alynkt <alynkirsch@gmail.com>
2022-12-08 01:23:45 -0500

    did create playist

--- static/script.js
+++ static/script.js
@@ -88,8 +88,78 @@ menuItems.forEach(
 
 function hideElement(context) {
   var hiddenElement = context.parentElement;
-  hiddenElement.style.display = "none";
+  hiddenElement.remove()
   
+}
+
+function addSong(){
+  searchedSong = document.getElementById("query").value;
+  if (searchedSong == "Dura"){
+    var target = document.getElementById("grid-container")
+    var figure = document.createElement("figure");
+    target.appendChild(figure);
+
+    var side_div = document.createElement("div");
+    side_div.classList.add("side-by-side");
+    figure.appendChild(side_div);
+
+    var image =document.createElement("img");
+    image.alt = "Dura by Daddy Yankee";
+    image.src = ".\\static\\dura.jpg";
+    side_div.appendChild(image);
+
+    var image_play = document.createElement("img");
+    image_play.classList.add("play");
+    image_play.alt = "play this song";
+    image_play.src = "..\\static\\play-solid.svg";
+    //image_play.onclick = changeplay(this);
+    side_div.appendChild(image_play);
+
+    var image_heart = document.createElement("img");
+    image_heart.classList.add("heart");
+    image_heart.alt = "like this song";
+    image_heart.src = ".\\static\\heart-regular.svg";
+    //image_heart.onclick = chngimg(this);
+    side_div.appendChild(image_heart);
+
+    var image_x = document.createElement("img");
+    image_x.classList.add("x");
+    image_x.alt = "get rid of this song";
+    image_x.src = "..\\static\\x-solid.svg";
+    //image_x.onclick = hideElement(this);
+    side_div.appendChild(image_x);
+
+    var white_div = document.createElement("div");
+    white_div.classList.add("white");
+    side_div.appendChild(white_div);
+
+    var figcaption = document.createElement("figcaption");
+    figcaption.innerHTML = "Dura";
+    white_div.appendChild(figcaption);
+
+    var p = document.createElement("p");
+    p.innerHTML = "Daddy Yankee";
+    white_div.appendChild(p);
+
+
+
+    //target.innerHTML += '<figure>\n<div class="side-by-side">\n<img alt="Dura by Daddy Yankee" src="..\static\dura.jpg">\n<img class="play" alt="play this song" src="..\static\play-solid.svg" onclick="changeplay(this)">\n<img class="heart" alt="like this song" src="..\static\heart-regular.svg" onclick="chngimg(this)">\n<img class="x" alt="get rid of this song" src="..\static\\x-solid.svg" onclick="hideElement(this)">\n<div class="white">\n<figcaption>Dura</figcaption>\n<p>Daddy Yankee</p>\n</div>\n</div>";'
+  }
+  /** 
+  else if (searchedSong == "TUDO BOM"){
+
+  }
+  else if (searchedSong == "Neverita"){
+
+  }
+  else if (searchedSong == "El Prestamo"){
+
+  }
+  else if (searchedSong == "Danza Kuduro"){
+
+  
+  }
+  */
 }
 
 
